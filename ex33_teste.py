0315b5069e01233644f0879bef466676e5e0cc19
ALMEYDA JUNIOR SANTOS COSTA <almeyda.junior.74@gmail.com>
2026-01-28 12:22:25 -0300

    tuplas

--- ex33_teste.py
+++ ex33_teste.py
@@ -1,1 +1,6 @@
-n=int("qual o valor extato")
+palavra=('arroz','pao','carne','suco')
+for p in palavra:
+    print(f"dentro da palavra {p} tem vogais", end="")
+    for letra in p:
+        if letra.lower() in"aeiou":
+            if letra.lower()
